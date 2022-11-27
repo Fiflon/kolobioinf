c0064f7c22aa5bb32c9875916fb8c5cf8518e631
madzixeq <118571976+madzixeq@users.noreply.github.com>
2022-11-27 19:30:33 +0100

    Add files via upload

---
+++ generator_z_sam.py
@@ -0,0 +1,46 @@
+from scipy.stats import genextreme
+import matplotlib.pyplot as plt
+import numpy as np
+from Bio import SeqIO
+from random import randint
+from Bio.SeqRecord import SeqRecord
+from io import StringIO
+
+
+ax = plt.subplot()
+
+c=-0.2
+
+r = genextreme.rvs(c,size=10000,loc=8000,scale=500)
+
+r=r.astype(int)
+ax.hist(r,bins=50, density=True, histtype="stepfilled")
+
+caly_genom = SeqIO.read("C:\\Users\\bloom\\Desktop\\kolobioinf-master\\kolobioinf-master\\sequence.fasta", "fasta")
+i = 0
+suma = 0
+fragmenty = []
+limit = len(caly_genom.seq)
+plik_sam = open("C:\\Users\\bloom\\Desktop\\kolobioinf-master\\kolobioinf-master\\fragmenty.sam","w")
+tmp=""
+plik_sam.write("@SQ\tSN:kng_reg01\tLN:1000000")
+for dlugosc in r:
+    if dlugosc < 0:
+        dlugosc = dlugosc * (-1)
+
+    if dlugosc >= limit:
+        dlugosc = randint(0, limit)
+    start = randint(0, limit - dlugosc)
+    end = start + dlugosc
+    fragment = caly_genom.seq[start:end]
+    record = SeqRecord(fragment, "odczyt_%i_s%i_d%i.fasta" % (i + 1,start,dlugosc), "", "")
+    fragmenty.append(record)
+    i = i+1
+    tmp="odczyt_"+str(i)+"_s"+str(start)+"_d"+str(dlugosc)+".fasta"
+    plik_sam.write(tmp+"\t0\tkng_reg01\t"+str(start)+"\t60\t"+str(dlugosc)+"M\t*\t0\t0\t"+str(fragment)+"\t*\n")
+    suma+=dlugosc
+    print(suma)
+    if suma>20000000:
+        break
+
+SeqIO.write(fragmenty, "C:\\Users\\bloom\\Desktop\\kolobioinf-master\\kolobioinf-master\\fragmenty.fasta", "fasta")
